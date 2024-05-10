7c697399b03f7276c2aceda646839ef8c0eee968
jcavenue <jcfababeir09@gmail.com>
2024-05-11 07:53:05 +0800

    Added testimonial review

--- app/_components/ColleagueReviews.tsx
+++ app/_components/ColleagueReviews.tsx
@@ -1,117 +1,49 @@
 import React from 'react';
 import Image from 'next/image';
 
+import review_items from '@/constant/testimonial_review';
+
 const ColleagueReviews = () => {
   return (
     <div className="container mx-auto">
       <div className="p-10">
-        <div className="my-5">
-          <div className="text-2xl font-semibold mb-2 text-center md:text-start">
+        <div className="my-5 text-center md:text-start">
+          <div className="text-2xl font-semibold mb-2 ">
             Testimonials
+          </div>
+          <div className=' text-slate-600 font-light'>
+            How my colleague says about me
           </div>
         </div>
         <div className="md:px-[40px] py-5">
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-[40px]">
-            <div>
-              <div className="text-sm mb-3 font-light italic">
-                &quot;JC is an approachable and helpful colleague. He guided me
-                on the development tools that we need to learn and understand.
-                He is responsible with his workload and has good management
-                skills.&quot;
-              </div>
-              <div className="flex flex-row gap-4">
-                <div>
-                  <div className="rounded-full overflow-hidden">
-                    <Image
-                      alt="Drexmir Review"
-                      width={45}
-                      height={45}
-                      src="/images/drexmir_review.png"
-                    />
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-[40px]">
+            {review_items.sort(() => Math.random() - 0.5).slice(0, 4).map((items) => {
+              return (
+                <div key={items.name}>
+                  <div className="text-sm mb-3 font-light italic">
+                    &quot;{items.description}&quot;
+                  </div>
+                  <div className="flex flex-row gap-4">
+                    <div>
+                      <div className="rounded-full overflow-hidden">
+                        <Image
+                          alt={`${items.name} Profile Picture`}
+                          width={45}
+                          height={45}
+                          src={`/images/${items.image_link}`}
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="text-blue-500">{items.name}</div>
+                      <div className="text-sm text-gray-400">
+                        {items.position}
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div>
-                  <div className="text-blue-500">Drexmir Mentiza</div>
-                  <div className="text-sm text-gray-400">
-                    Former Colleague at AWB
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="text-sm mb-3 font-light italic">
-                &quot;I can say that he is a reliable software engineer. He is
-                great to work with and has leadership characteristics.&quot;
-              </div>
-              <div className="flex flex-row gap-4">
-                <div>
-                  <div className="rounded-full overflow-hidden">
-                    <Image
-                      alt="Marcy Review"
-                      width={45}
-                      height={45}
-                      src="/images/marcy_review.png"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div className="text-blue-500">Marcy Villegas</div>
-                  <div className="text-sm text-gray-400">
-                    Former Colleague at MVI
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="text-sm mb-3 font-light italic">
-                &quot;I witnessed his journey from being an intern to becoming a
-                freelance QA and I can say that he is hardworking and dedicated
-                to his work.&quot;
-              </div>
-              <div className="flex flex-row gap-4">
-                <div>
-                  <div className="rounded-full overflow-hidden">
-                    <Image
-                      alt="Angel Review"
-                      width={45}
-                      height={45}
-                      src="/images/angel_review.png"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div className="text-blue-500">Angelica Dacuplo</div>
-                  <div className="text-sm text-gray-400">
-                    Former QA Lead at AWB
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="text-sm mb-3 font-light italic">
-                &quot;I had the opportunity to work with JC at AWB. We were both
-                interns, and I observed how committed he was to producing
-                high-quality work as a developer or QA.&quot;
-              </div>
-              <div className="flex flex-row gap-4">
-                <div>
-                  <div className="rounded-full overflow-hidden">
-                    <Image
-                      alt="Krystal Review"
-                      width={45}
-                      height={45}
-                      src="/images/krystal_review.png"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div className="text-blue-500">Krystal Yvette Villapando</div>
-                  <div className="text-sm text-gray-400">
-                    Former Colleague at AWB
-                  </div>
-                </div>
-              </div>
-            </div>
+              );
+            })}
           </div>
         </div>
       </div>
---
+++ constant/testimonial_review.tsx
@@ -0,0 +1,40 @@
+const review_items = [
+  {
+    description:'JC is an approachable and helpful colleague. He guided me on the development tools that we need to learn and understand. He is responsible with his workload and has good management skills.',
+    name: 'Drexmir Mentiza',
+    position: 'Business Analyst',
+    image_link: 'drexmir.png',
+  },
+  {
+    description: 'I can say that he is a reliable software engineer. He is great to work with and has leadership characteristics.',
+    name: 'Marcy Villegas',
+    position: 'Software Engineer',
+    image_link: 'marcy.png',
+  },
+  {
+    description: 'I witnessed his journey from being an intern to becoming a freelance QA and I can say that he is hardworking and dedicated to his work.',
+    name: 'Angelica Daguplo',
+    position: 'Lead Software Quality Engineer',
+    image_link: 'angel.png',
+  },
+  {
+    description: 'I had the opportunity to work with JC at AWB. We were both interns, and I observed how committed he was to producing high-quality work as a developer or QA.',
+    name: 'Krystal Yvette Villapando',
+    position: 'Software Quality Engineer',
+    image_link: 'krystal.png',
+  },
+  {
+    description: 'He has demonstrated initiative, reliability, and an authentic drive to create through code. His affinity for work and collaboration is a boon to any team he joins.',
+    name: 'Eduard Jered Presas',
+    position: 'Technical Project Manager',
+    image_link: 'eduard.png',
+  },
+  {
+    description: 'With the opportunity of working with JC you can clearly see that he has good leadership qualities. His initiative is what makes him stands out along with his dedication to get the job done.',
+    name: 'James De Leon',
+    position: 'Software Engineer',
+    image_link: 'james.png',
+  },
+];
+
+export default review_items
